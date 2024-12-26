f30398c658d89d6dc26a9c0635a4e3e00fb70476
syedsaudsaad <s.saud15065@gmail.com>
2024-12-26 16:00:49 +0500

    about-3

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
-import Hero from "./Components/Hero/Hero";
-import Skill from "./Components/Skill/Skill";
-import Experience from "./Components/Experience/Experience";
-import Work from "./Components/Work/Work";
+
 import About from "./Components/About/About";
-import Contact from "./Components/Contact/Contact";
-import projects from "./Projects/page"
-import Project from "./Components/Project/Project";
 
 export default function Home() {
   return (
     <div>
-      <Hero />
-      <Skill />
-      <Experience />
-      <Work />
+      
       <About />
-      <Contact />
-      <Project />
+      
 
     </div>
   )
